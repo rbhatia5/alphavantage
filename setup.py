039e60da1e7f33cbbcb741bcbaac38d1c6dfea1c
Rahul Bhatia <rahul.bhatia.k@gmail.com>
2017-03-30 20:52:25 -0400

    Removing some .pyc files and renaming timeseries

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 from distutils.core import setup
 
 setup(name='alphavantage',
-      version='0.1',
-      description='AlphaVantage API',
-      author='Rahul Bhatia',
-      author_email='rahul.bhatia.k@gmail.com',
-      packages=['alphavantage'],
-     )
+    version='0.1.0',
+    description='AlphaVantage API',
+    author='Rahul Bhatia',
+    author_email='rahul.bhatia.k@gmail.com',
+    packages=['alphavantage'],
+    install_requries=[]
+)
+
